f1a81b9b8b552de3681a543e3516e6af9522c030
jim707t <jimgamerwinner@gmail.com>
2023-01-20 19:12:19 -0800

    small edit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -32,33 +32,33 @@ export default function about() {
         About me
       </Heading>
       <Divider marginTop="5" />
-      <Box margin="auto" display="flex" justifyContent="center" p={{base: 10, md:25}}>
+      <Box margin="auto" display="flex" justifyContent="center" p={{base: 10, md:20}}>
       <Text>
       {data.about.text}
       </Text>
      </Box>
-        <VStack p={10}>
+      <VStack p={5}>
     <List spacing={3}>
-      <Text>Find me on</Text>
+      <Text>Find me on:</Text>
         <ListItem>
           <ListIcon as={BsGithub} color='blue.400' />
-            <Link color='teal.500' href='https://github.com/Jim707t' isExternal> @github <ExternalLinkIcon mx='2px' /></Link> where i share code and contribute
+            <Link color='teal.500' href='https://github.com/Jim707t' isExternal> Github(Jim707t) <ExternalLinkIcon mx='2px' /></Link>
          </ListItem>
         <ListItem>
        <ListIcon as={BsReddit} color='blue.400' />
-       <Link color='teal.500' href='https://reddit.com/u/Jim707t' isExternal> @reddit <ExternalLinkIcon mx='2px' /></Link> for chill and news
+       <Link color='teal.500' href='https://reddit.com/u/Jim707t' isExternal> Reddit(Jim707t) <ExternalLinkIcon mx='2px' /></Link> 
       </ListItem>
       <ListItem>
           <ListIcon as={BsMastodon} color='blue.400' />
-        <Link color='teal.500' href='https://cyberplace.social/@Jim' isExternal> @mastodon <ExternalLinkIcon mx='2px' /></Link> It's a cool DSMN
+        <Link color='teal.500' href='https://cyberplace.social/@Jim' isExternal> Mastodon(Jim) <ExternalLinkIcon mx='2px' /></Link> 
        </ListItem>
        <ListItem>
       <ListIcon as={BsTwitter} color='blue.400' />
-      <Link color='teal.500' href='https://twitter.com/J1mQuincy' isExternal> @twitter <ExternalLinkIcon mx='2px' /></Link> no reason
+      <Link color='teal.500' href='https://twitter.com/J1mQuincy' isExternal> Twitter(J1mQuincy) <ExternalLinkIcon mx='2px' /></Link> 
       </ListItem>
       <ListItem>
       <ListIcon as={BsInstagram} color='blue.400' />
-      <Link color='teal.500' href='https://instagram.com/j1mquincy' isExternal> @instagram <ExternalLinkIcon mx='2px' /></Link> for sharing some pretty stuff
+      <Link color='teal.500' href='https://instagram.com/j1mquincy' isExternal> Instagram(J1mQuincy) <ExternalLinkIcon mx='2px' /></Link> 
      </ListItem>
 
     </List>
